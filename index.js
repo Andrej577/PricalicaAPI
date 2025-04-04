240b30575be4ea7858957a6cb21c387cb2a4a01d
Andrej577 <andrej.git@yahoo.com>
2025-04-04 17:55:44 +0200

    - popunjena baza podataka

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { testConnection } = require('./DBContext/DB');
+
 app.get('/', (req, res) => {
-    res.send('Pozdrav iz Node.js aplikacije');
+    res.send('Pozdrav iz Node.js aplikacije2');
 });
+
+
+testConnection();
 
 app.listen(PORT, () => {
     console.log(`Server radi na http://localhost:${PORT}`);
