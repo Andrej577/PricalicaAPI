4a745f193c935f1093d5b10d7131e22e7bf1fd8a
Andrej Jakarić <andrej.git@yahoo.com>
2026-03-22 17:08:57 +0100

    - fix dohvata detalja knjige

--- Routes/knjige.js
+++ Routes/knjige.js
@@ -13,19 +13,31 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    const knjigaId = req.params.id; 
+    const knjigaId = req.params.id;
+
     try {
-        const [rows] = await db.pool.query('SELECT * FROM knjige WHERE knjiga_id = ?', [knjigaId]);
+        const [rows] = await db.pool.query(`
+            SELECT 
+                k.naslov, 
+                CONCAT(a.ime, ' ', a.prezime) AS autor, 
+                z.nazivZanra AS zanr,
+                k.opis
+            FROM knjige k
+            INNER JOIN korisnici a ON a.korisnik_id = k.autor_id
+            INNER JOIN zanrovi z ON z.zanr_id = k.zanr_id
+            WHERE k.knjiga_id = ?;
+        `, [knjigaId]);
+
         if (rows.length === 0) {
             return res.status(404).json({ error: 'Knjiga nije pronađena' });
         }
+
         res.json(rows[0]);
     } catch (err) {
         console.error('Greška pri dohvaćanju knjige:', err);
         res.status(500).json({ error: 'Greška na serveru' });
     }
 });
-
 
 router.delete('/:id', async (req, res) =>
 {
